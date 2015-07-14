c4749711c4311f4530d1d19ac6052f03b01e98f8
azuretower <ryan.merrill215@gmail.com>
2015-07-14 16:05:01 -0600

    changed views.py

--- main/views.py
+++ main/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from main.models import Film
+from main.models import Film, Director, Actor, Actress
 # Create your views here.
 
 
 def first_view(request):
     text_string = ''
 
-    films = Film.objects.filter(title__startswith='Z')
+    films = Film.objects.all()
 
-    for film in films:
-        text_string += 'Title: %s | Year: %s </br>' % (film.title, film.year)
+    directors = Director.objects.filter(name='Abrahams, Jim')
+
+    for director in directors:
+        films = director.film_set.all()
+
+        for film in films:
+            text_string += 'Title: %s  | Director: %s </br>' % (film.title, director.name)
 
     return HttpResponse(text_string)
